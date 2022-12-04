7b58f50d8a224fae99332792fd7e9fcf9ef289b9
nadine.zingano@gmail.com <downton098>
2022-12-04 13:58:11 -0300

    Lab form verifications

--- drugstore/drugstore/Models/Lab.cs
+++ drugstore/drugstore/Models/Lab.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,27 @@ namespace drugstore.Models
     public class Lab
     {
         public int Id { get; set; }
+
+        [Display(Name = "Trading Name")]
+        [Required(ErrorMessage = "Required Field")]
+        [MinLength(3, ErrorMessage = "Name must be more than 3 characters"), MaxLength(35, ErrorMessage = "Name must be less than 35 characters")]
         public string Name { get; set; }
+
+        [Display(Name = "Company Name")]
+        [Required(ErrorMessage = "Required Field")]
+        [MinLength(3, ErrorMessage = "Name must be more than 3 characters"), MaxLength(35, ErrorMessage = "Name must be less than 35 characters")]
         public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "Required Field")]
         public string Address { get; set; }
+
+        [Display(Name = "Telephone Number")]
+        [Required(ErrorMessage = "Required Field")]
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Please enter valid phone no.")]
         public string Telephone { get; set; }
+
+        [Required(ErrorMessage = "Required Field")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid format.")]
         public string Email { get; set; }
 
         public ICollection<Medicine> Medicines = new List<Medicine>();
